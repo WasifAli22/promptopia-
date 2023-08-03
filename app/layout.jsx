eebc61aea886940a7a5b14a3db525735dcab82c2
Wasif Shahid <wasifaliwasif22@gmail.com>
2023-08-03 22:15:34 +0500

    necessary component created

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import "@styles/global.css"
-
+import Nav from "@components/Nav"
+import Provider from "@components/Provider"
 export const metadata = {
     title: "Promptopia",
     description: "discover and share AI prompts"
@@ -12,6 +13,7 @@ const RootLayout = ({ children }) => {
                     <div className="gradient" />
                 </div>
                 <main className="app">
+                    <Nav />
                     {children}
                 </main>
             </body>
